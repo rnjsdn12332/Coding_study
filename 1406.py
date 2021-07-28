9286df6f2bf51dfcf8274e10a4f2549a3ce71a3c
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2021-07-29 02:49:44 +0900

    Update 1406.py

--- 1406.py
+++ 1406.py
@@ -1,3 +1,4 @@
+'''
 import sys
 input=sys.stdin.readline
 
@@ -24,3 +25,29 @@ for i in range(n) :
         idx+=1
 
 print(('').join(s))
+'''
+
+#위 코드는 시간초과 문제 발생 시간복잡도 생각해서 insert를 빼고 pop, append로 짰더니 해결
+
+import sys
+input=sys.stdin.readline
+
+s=list(input().strip())
+n=int(input().strip())
+stack2=[]
+
+idx=len(s)
+for i in range(n) :
+    order=list(input().strip().split())
+
+
+    if order[0] == 'L' and s != []:
+        stack2.append(s.pop())
+    elif order[0] == 'D' and stack2 != []:
+        s.append(stack2.pop())
+    elif order[0] == 'B' and s != []:
+        s.pop()
+    elif order[0] == 'P' :
+        s.append(order[1])
+
+print("".join(s+list(reversed(stack2))))
