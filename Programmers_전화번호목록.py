510bdaf8df03d228752313d57610f41eb56189ba
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-08-20 00:40:48 +0900

    Update Programmers_전화번호목록.py

--- Programmers_전화번호목록.py
+++ Programmers_전화번호목록.py
@@ -1,7 +1,8 @@
 def solution(phone_book):
-    answer = True
-    for i in range(len(phone_book)) :
-        for j in range(i+1, len(phone_book)) :
-            if phone_book[i] in phone_book[j] :
-                return False
-    return answer
+    
+    phone_book.sort()
+    for i in range(len(phone_book)-1): 
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])] : 
+            return False
+
+    return True
