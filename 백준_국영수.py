1c26e9f7f438d6fe9aed980c9a081170b5f58563
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-09-01 19:17:24 +0900

    Update 백준_국영수.py

--- 백준_국영수.py
+++ 백준_국영수.py
@@ -5,7 +5,7 @@ array=[]
 for _ in range(n) :
     array.append(input().split())
     
-array.sort(key=lambda x:(-int(x[1]), int(x[2]), -int(x[2]), x[0]))
+array.sort(key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for temp in array :
     print(temp[0])
