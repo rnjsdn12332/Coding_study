c210b1a8dbb8e9304d7490f922019cd83de91637
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-10-09 17:39:45 +0900

    Create Programmers_정수삼각형.py

---
+++ Programmers_정수삼각형.py
@@ -0,0 +1,11 @@
+
+def solution(triangle):
+
+    for i in range(1, len(triangle)) :
+        for j in range(1, i) :
+            triangle[i][j]+=max(triangle[i-1][j-1], triangle[i-1][j])
+            
+        triangle[i][0]+=triangle[i-1][0]
+        triangle[i][-1]+=triangle[i-1][-1]
+        
+    return max(triangle[-1])
