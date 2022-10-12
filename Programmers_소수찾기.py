5656b0ea37b5a8c6a0327c23d327c5ebf60e8e7c
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-10-12 23:59:11 +0900

    Create Programmers_소수찾기.py

---
+++ Programmers_소수찾기.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = 0
+    array=[]
+    def find(x) :
+        for i in range(2, math.sqrt(x)+1) :
+            if x%i !=0 :
+                return True
+    
+    
+    
+    
+    
+        
+    return answer
