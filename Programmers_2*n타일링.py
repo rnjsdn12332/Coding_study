6a1a48e1961a04afed66c323c47678e70d526e8b
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-10-26 23:52:11 +0900

    Update Programmers_2*n타일링.py

--- Programmers_2*n타일링.py
+++ Programmers_2*n타일링.py
@@ -1,8 +1,15 @@
 def solution(n):
-    answer = 1
-    if n%2==0 :
-        answer+=2**(n/2)
-    else :
-        answer+=2**((n-1)/2)
-        
-    return answer%1000000007
+    answer = 0
+    max_val=n//2
+    one=1
+    two=2
+    if n==1:
+        return one
+    elif n==2:
+        return two
+    for i in range(n-2):
+        temp=one
+        one=two
+        two=one+temp
+    answer=two%1000000007
+    return answer
