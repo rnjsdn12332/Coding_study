738162246a5bfe855a3663e27d04a30074bdb0a3
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-10-30 18:10:50 +0900

    Create 우테코_3번.py

---
+++ 우테코_3번.py
@@ -0,0 +1,15 @@
+
+#숫자 입력
+number=input()
+#손뼉치는 횟수 저장할 변수
+cnt=0
+
+#주어진 숫자까지 1부터 차례대로 반복문
+for num in range(1,int(number)+1) :
+    #3, 6, 9 들어가있을때
+    if "3" in str(num) or "6" in str(num) or "9" in str(num) :
+        #카운트 셈
+
+        cnt+=1
+
+print(cnt)
